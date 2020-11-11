2f2303af417819c1a6da936e05965dcc69712250
niksamoyloff <niksamoyloff@gmail.com>
2020-11-11 12:09:55 +0300

    Add class to parse connection string to OIC main server

---
+++ PrognozMdp/Services/OicConnectionStringParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrognozMdp.Services
+{
+    public class OicConnectionStringParser
+    {
+        private readonly Dictionary<string, string> _fields;
+
+        public string Server => _fields.ContainsKey("Server") ? _fields["Server"] : null;
+
+        public string Database => _fields.ContainsKey("Database") ? _fields["Database"] : null;
+
+        public string UserId => _fields.ContainsKey("User Id") ? _fields["User Id"] : null;
+
+        public string Password => _fields.ContainsKey("Password") ? _fields["Password"] : null;
+
+        public string IntegratedSecurity => _fields.ContainsKey("Integrated Security") ? _fields["Integrated Security"] : null;
+
+        public OicConnectionStringParser(string connectionString)
+        {
+            if (connectionString != null)
+                _fields = connectionString.Split(';').ToDictionary(s => s.Split('=')[0], s => s.Split('=')[1]);
+        }
+    }
+}
+
