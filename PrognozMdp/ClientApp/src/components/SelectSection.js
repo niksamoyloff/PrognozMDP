dfcde783230fdb086c0bab36cdf944f3df4e954e
niksamoyloff <niksamoyloff@gmail.com>
2020-11-11 10:50:40 +0300

    Create SelectSection component by using hooks, 'react-select' and 'axios' packages

---
+++ PrognozMdp/ClientApp/src/components/SelectSection.js
@@ -0,0 +1,35 @@
+﻿import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
+import axios from 'axios';
+
+function SelectSection(props) {
+    const [sections, setSections] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchSections = async () => {
+            setIsLoading(true);
+            const result = await axios(
+                'SimplifiedAnalysis/GetSections',
+            );
+            setSections(result);
+            setIsLoading(false);
+        };
+        fetchSections();
+    }, []);
+
+    function handleChangeSelect(e) {
+        props.onChangeSectionId(e.value);
+    }
+
+    return (
+        <Select
+            options={sections.data}
+            isLoading={isLoading}
+            placeholder="Выберите сечение..."
+            noOptionsMessage={() => "Сечения отсутствуют."}
+            onChange={handleChangeSelect}/>
+    );
+};
+
+export default SelectSection;
