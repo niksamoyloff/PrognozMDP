8a3676301513d3ca4489761393071b41d7fd32a6
niksamoyloff <niksamoyloff@gmail.com>
2020-11-11 11:54:17 +0300

    Create controller for simplified analysis. Add GetSections() and GetEquipmentBySection() methods

---
+++ PrognozMdp/Controllers/SimplifiedAnalysisController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using PrognozMdp.Services;
+
+namespace PrognozMdp.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class SimplifiedAnalysisController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly OIC _oic;
+        public SimplifiedAnalysisController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _oic  = new OIC(_configuration);
+        }
+
+        [HttpGet]
+        public JObject[] GetSections()
+        {
+            DataRowCollection sectionCollection = _oic.GetSectionsFromOic();
+            var sectionList = new List<JObject>();
+
+            if (sectionCollection != null && sectionCollection.Count > 0)
+            {
+                foreach (DataRow row in sectionCollection)
+                {
+                    var jObject = JObject.FromObject(new
+                    {
+                        value = row.ItemArray[0],
+                        label = row.ItemArray[1]
+                    });
+                    sectionList.Add(jObject);
+                }
+            }
+            return sectionList.ToArray();
+        }
+
+        [HttpPost]
+        public JObject[] GetEquipmentBySection(string sectionId)
+        {
+            if (string.IsNullOrEmpty(sectionId))
+                return null;
+            DataRowCollection eqCollection = _oic.GetEquipmentBySectionFromOic(sectionId);
+            var eqList = new List<JObject>();
+
+            if (eqCollection != null && eqCollection.Count > 0)
+            {
+                foreach (DataRow row in eqCollection)
+                {
+                    var jObject = JObject.FromObject(new
+                    {
+                        idEq = row.ItemArray[0],
+                        typeEq = row.ItemArray[1],
+                        nameEq = row.ItemArray[2],
+                        tsId = row.ItemArray[3]
+                    });
+                    eqList.Add(jObject);
+                }
+            }
+            return eqList.ToArray();
+        }
+    }
+}
