12574f66e9403ec4d63e615ed61225d2de8c0497
niksamoyloff <niksamoyloff@gmail.com>
2020-11-17 19:51:24 +0300

    Add StateToggleButton component for last column cell. Init equipmentToCalc array.

--- PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/EquipmentTable.js
+++ PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/EquipmentTable.js
@@ -1,13 +1,19 @@
 ﻿import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import ReactTable from 'react-table-6';
-import StateButton from './EquipmentStateToggleButton';
+import StateToggleButton from './StateToggleButton';
 import 'react-table-6/react-table.css';
 import './EquipmentTable.css';
 
 function EquipmentTable(props) {
     const [equipment, setEquipment] = useState([]);
+    const [equipmentToCalc, setEquipmentToCalc] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const states = [
+        { name: "Вкл", value: "1" },
+        { name: "Откл", value: "0" }
+    ];
+    const variants = ["outline-success","outline-danger"];
 
     useEffect(() => {
         const fetchEquipment = async () => {
@@ -21,11 +27,19 @@ function EquipmentTable(props) {
                 }
             );
             setEquipment(result.data);
+            setEquipmentToCalc(result.data.map(obj => {
+                let objToCalc = {}
+                objToCalc.nameEq = obj.nameEq;
+                objToCalc.isEnabled = obj.isEnabled;
+                return objToCalc;
+            }
+            ));
+            console.log(equipmentToCalc);
             setIsLoading(false);
         };
         if (props.sectionId !== null && props.sectionId !== "")
             fetchEquipment();
-    }, [props]);
+    }, [props.sectionId]);
 
     const columns = useMemo(
         () => [
@@ -33,11 +47,13 @@ function EquipmentTable(props) {
                 Header: () => <b>Тип</b>,
                 accessor: 'typeEq',
                 minWidth: 100,
-                width: 100
+                width: 100,
+                Footer: () => <b>Всего: {equipment.length}</b>
             },
             {
                 Header: () => <b>Название оборудования</b>,
-                accessor: 'nameEq'
+                accessor: 'nameEq',
+                style: { 'whiteSpace': 'normal' }
             },
             {
                 Header: () => <b>ТС</b>,
@@ -49,10 +65,20 @@ function EquipmentTable(props) {
                 Header: () => <b>Состояние</b>,
                 minWidth: 100,
                 width: 100,
-                Cell: () => <StateButton/>
-            }
+                Cell: (row) => <StateToggleButton 
+                                    key={row.index + '-state-button'} 
+                                    radios={states}
+                                    variants={variants}
+                                    size="sm"
+                                    defaultState={row.original.isEnabled} 
+                                    onChangeState={() => row.original.isEnabled = !row.original.isEnabled}/>
+            }   
             ],
-        []
+        [
+            equipment.length, 
+            states,
+            variants
+        ]
     );
 
     return (
@@ -61,15 +87,17 @@ function EquipmentTable(props) {
                 columns={columns} 
                 data={equipment} 
                 loading={isLoading}
-                defaultPageSize={20}
-                previousText="Предыдущая"
-                nextText="Следующая"
+                sortable={true}
+                showPagination={false}
+                pageSize={equipment.length}
                 loadingText="Поиск оборудования..."
                 noDataText={equipment.length === 0 ? "Оборудование не найдено." : ""}
-                pageText="Страница"
-                ofText="из"
-                rowsText="строк"
-                pageSizeOptions={[10, 15, 20, 25, 50]}
+                //previousText="Предыдущая"
+                //nextText="Следующая"
+                //pageText="Страница"
+                //ofText="из"
+                //rowsText="строк"
+                //pageSizeOptions={[10, 15, 20, 25, 50]}
             />
         </div>
     );
