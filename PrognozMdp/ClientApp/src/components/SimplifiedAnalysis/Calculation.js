25e1f4ac420072aa8e3fb8a824c67a2e263cfd29
niksamoyloff <niksamoyloff@gmail.com>
2020-11-17 19:43:51 +0300

    Add and init Calculation component and styling him

---
+++ PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/Calculation.js
@@ -0,0 +1,40 @@
+﻿import React, { Fragment, useState } from 'react';
+import DtPicker from './DtPicker';
+import StateToggleButton from './StateToggleButton';
+import CalcButton from './CalcButton';
+import './Calculation.css';
+import './DtPicker.css';
+
+function Calculation() {
+    const states = [
+        { name: "МДП", value: "1" },
+        { name: "АДП", value: "0" }
+    ];
+    const variants = ["outline-warning","outline-danger"];
+
+    return (
+        <Fragment>
+            <div className="calculation">
+                <div className="select-flow">
+                    <span><b>Вычислить: </b></span>
+                    <StateToggleButton
+                        key="select-flow"
+                        radios={states}
+                        variants={variants}
+                        defaultState={true} 
+                        //onChangeState={}
+
+                    />
+                </div>
+                <div className="select-time">
+                    <DtPicker />
+                </div>
+                <div className="calc-btn">
+                    <CalcButton />
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+export default Calculation;
