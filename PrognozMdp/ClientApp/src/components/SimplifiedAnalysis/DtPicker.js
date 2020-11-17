719583d08de6f7817a8e316b83bbc2475cb53a3d
niksamoyloff <niksamoyloff@gmail.com>
2020-11-17 19:44:09 +0300

    Add and init DtPicker component and styling him

---
+++ PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/DtPicker.js
@@ -0,0 +1,35 @@
+﻿import React, { Fragment, useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
+import { Form } from 'react-bootstrap';
+
+function DtPicker() {
+    const [value, onChange] = useState(new Date());
+    const [checked, setChecked] = useState(true);
+
+    return (
+        <Fragment>
+                <Form>
+                    <Form.Check 
+                        id="currentTimeCheckbox"
+                        type="checkbox"
+                        checked={checked}
+                        label={<b>Текущее время</b>}
+                        onChange={() => setChecked(!checked)}
+                    />
+                    {
+                        !checked ?
+                            <DateTimePicker
+                                onChange={onChange}
+                                value={value}
+                                format="dd.MM.yyyy HH:mm:ss"
+                                disableClock={true}
+                            /> :
+                            null
+                    }
+                </Form>
+        </Fragment>
+        
+    );
+}
+
+export default DtPicker;
