a3d930f9a8d1f19aed4d885fce0f8911257f4d9d
niksamoyloff <niksamoyloff@gmail.com>
2020-11-28 11:16:28 +0300

    Fix onChange event

--- PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/DtPicker.js
+++ PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/DtPicker.js
@@ -2,7 +2,7 @@
 import DateTimePicker from 'react-datetime-picker';
 import { Form } from 'react-bootstrap';
 
-function DtPicker() {
+function DtPicker(props) {
     const [value, onChange] = useState(new Date());
     const [checked, setChecked] = useState(true);
 
@@ -14,12 +14,18 @@ function DtPicker() {
                         type="checkbox"
                         checked={checked}
                         label={<b>Текущее время</b>}
-                        onChange={() => setChecked(!checked)}
+                        onChange={() => {
+                            setChecked(!checked);
+                            props.onChangeDt(new Date());
+                        }}
                     />
                     {
                         !checked ?
                             <DateTimePicker
-                                onChange={onChange}
+                                onChange={(val) => {
+                                    onChange(val);
+                                    props.onChangeDt(val);
+                                }}
                                 value={value}
                                 format="dd.MM.yyyy HH:mm:ss"
                                 disableClock={true}
