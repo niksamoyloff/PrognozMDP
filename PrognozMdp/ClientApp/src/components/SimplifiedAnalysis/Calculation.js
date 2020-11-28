042861f7105835d55c6872ccd59db410f45a19fe
niksamoyloff <niksamoyloff@gmail.com>
2020-11-28 11:15:51 +0300

    Add handles of change child components

--- PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/Calculation.js
+++ PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/Calculation.js
@@ -5,12 +5,35 @@ import CalcButton from './CalcButton';
 import './Calculation.css';
 import './DtPicker.css';
 
-function Calculation() {
-    const states = [
+function Calculation(props) {
+    const [flow, setFlow] = useState("1");
+    const [dt, setDt] = useState(new Date());
+    //const [flowWithVals, setFlowWithVals] = useState();
+    const [isLoading, setLoading] = useState(false);
+    const flows = [
         { name: "МДП", value: "1" },
         { name: "АДП", value: "0" }
     ];
     const variants = ["outline-warning","outline-danger"];
+
+    function handleChangeDt(newDt) {
+        setDt(newDt);
+    }
+
+    function handleGetFlow(newFlow) {
+        if (isLoading) {
+            props.onChangeFlowWithVals(newFlow);
+        }
+    }
+
+    function handleOnLoading(loading) {
+        setLoading(loading);
+        props.onLoading(loading);
+        if (isLoading) {
+            props.onChangeDt(dt);
+            props.onChangeFlow(flow ? "МДП" : "АДП");
+        }
+    }
 
     return (
         <Fragment>
@@ -19,18 +42,24 @@ function Calculation() {
                     <span><b>Вычислить: </b></span>
                     <StateToggleButton
                         key="select-flow"
-                        radios={states}
+                        radios={flows}
                         variants={variants}
                         defaultState={true} 
-                        //onChangeState={}
-
+                        onChangeState={() => setFlow(!flow)}
                     />
                 </div>
                 <div className="select-time">
-                    <DtPicker />
+                    <DtPicker onChangeDt={handleChangeDt}/>
                 </div>
                 <div className="calc-btn">
-                    <CalcButton />
+                    <CalcButton 
+                        flow={flow ? "mdp" : "adp"}
+                        dt={dt}
+                        sectId={props.sectionId}
+                        bitMask={props.bitMask}
+                        onGetFlow={handleGetFlow}
+                        onLoading={handleOnLoading}
+                    />
                 </div>
             </div>
         </Fragment>
