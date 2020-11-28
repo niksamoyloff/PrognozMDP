6a5fb0a38b2551df0daef10dd2bc38fdd48226b4
niksamoyloff <niksamoyloff@gmail.com>
2020-11-28 11:01:11 +0300

    Add and init calculation result component

---
+++ PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/CalcResult.js
@@ -0,0 +1,30 @@
+﻿import React, { Fragment, useState } from 'react';
+import moment from 'moment'
+
+function CalcResult(props) {
+    const [isLoading, setLoading] = useState(props.loading);
+
+    return (
+        <Fragment>
+            {
+                !props.isClear
+            ?
+                props.flowWithVals !== undefined && props.flowWithVals !== null
+                    ? props.flowWithVals.flowCalcValue !== null
+                    ? <div className="result-calc">    
+                                <b>{props.flow}</b> в КС <b>{props.section}</b> для схемы <b>[</b><em>{props.disEq !== "" ? props.disEq : "Нормальная схема"}</em><b>]</b> на момент <b>{
+                                    moment(props.dt).format("HH:mm:ss DD.MM.yyyy")}</b> составляет <b style={
+                                 props.flowWithVals.flowCalcValue > props.flowWithVals.flowCurrentValue ? { color: 'red' } : ""}>{props.flowWithVals
+                                     .flowCalcValue.toFixed(2)}</b> МВт.
+                      </div>
+                    : <div>
+                            <span><b>Ремонт неинструктивный.</b></span>        
+                        </div>
+                    : ""
+            : ""
+            }
+        </Fragment>
+    );
+}
+
+export default CalcResult;
