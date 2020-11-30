96c950e098580896444ca63d3e7aa0ed042cc786
niksamoyloff <niksamoyloff@gmail.com>
2020-11-30 10:28:59 +0300

    Create and init Error component

---
+++ PrognozMdp/ClientApp/src/components/Error.js
@@ -0,0 +1,26 @@
+﻿import React, { Fragment, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+function Error() {
+    const [show, setShow] = useState(true);
+
+    const handleClose = () => setShow(false);
+
+    return (
+        <Fragment>
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Ошибка</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Произошла ошибка при получении данных!<br/>Попробуйте еще раз или обратитесь к администратору.</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Закрыть
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </Fragment>
+    );
+}
+
+export default Error;
