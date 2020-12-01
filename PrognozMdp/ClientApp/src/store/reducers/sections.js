c435db696eeb29a8570d79c7453668ac629e7408
niksamoyloff <niksamoyloff@gmail.com>
2020-12-01 16:10:12 +0300

    Add and init sections reducer

---
+++ PrognozMdp/ClientApp/src/store/reducers/sections.js
@@ -0,0 +1,32 @@
+﻿import * as types from '../constants/ActionTypes';
+
+const initialState = {
+    loading: false,
+    items: [],
+    error: null
+}
+
+export default function sections(state = initialState, action) {
+    switch (action.type) {
+        case types.FETCH_SECTIONS_STARTED:
+            return {
+                 ...state, 
+                 loading: true
+            };
+        case types.FETCH_SECTIONS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                items: [...state.items, action.payload]
+            };
+        case types.FETCH_SECTIONS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload.error
+            };
+        default:
+            return state;
+    }
+}
