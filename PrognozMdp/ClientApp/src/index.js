7627f0a39fb824d50f134298dede33c3b76431ae
niksamoyloff <niksamoyloff@gmail.com>
2020-12-01 16:11:11 +0300

    Use store and Provider

--- PrognozMdp/ClientApp/src/index.js
+++ PrognozMdp/ClientApp/src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -11,9 +13,11 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter basename={baseUrl}>
+        <App />
+      </BrowserRouter>
+    </Provider>,
   rootElement);
 
 registerServiceWorker();
