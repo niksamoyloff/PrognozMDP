6b9a66ddfcc26842673463445c369c6dfd2d0777
niksamoyloff <niksamoyloff@gmail.com>
2020-12-01 16:09:00 +0300

    Add and init fetch sections action

---
+++ PrognozMdp/ClientApp/src/store/actions/index.js
@@ -0,0 +1,35 @@
+﻿import * as types from '../constants/ActionTypes'
+import axios from 'axios';
+
+export const fetchSections = () => {
+    return async (dispatch, getState) => {
+        dispatch(fetchSectionsStarted());
+        console.log('current state:', getState());
+        await axios.get('SimplifiedAnalysis/GetSections')
+            .then(res => {
+                dispatch(fetchSectionsSuccess(res.data));
+                console.log('current state:', getState());
+            })
+            .catch(err => {
+                dispatch(fetchSectionsFailure(err));
+            });
+    };
+};
+
+const fetchSectionsStarted = () => ({
+    type: types.FETCH_SECTIONS_STARTED
+});
+
+const fetchSectionsSuccess = data => ({
+    type: types.FETCH_SECTIONS_SUCCESS,
+    payload: {
+        ...data
+    }
+});
+
+const fetchSectionsFailure = error => ({
+    type: types.FETCH_SECTIONS_FAILURE,
+    payload: {
+        error
+    }
+})
