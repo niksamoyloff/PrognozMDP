2e3cfe7d2ae9ec12a715a1bac55f93abb26b99ae
niksamoyloff <niksamoyloff@gmail.com>
2020-12-02 17:44:06 +0300

    Add and init equipment reducer

---
+++ PrognozMdp/ClientApp/src/store/reducers/equipment.js
@@ -0,0 +1,33 @@
+﻿import * as types from '../constants/ActionTypes';
+
+const initialState = {
+    loading: false,
+    items: [],
+    error: null
+}
+
+export default function equipmentReducer(state = initialState, action) {
+    switch (action.type) {
+        case types.FETCH_EQUIPMENT_STARTED:
+            return {
+                ...state, 
+                loading: true
+            };
+        case types.FETCH_EQUIPMENT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                items: action.payload.data
+            };
+        case types.FETCH_EQUIPMENT_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload.error,
+                items: []
+            };
+        default:
+            return state;
+    }
+}
