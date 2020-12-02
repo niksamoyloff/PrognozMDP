8d35069c3e4074612c50b499ae7bbc9400866424
niksamoyloff <niksamoyloff@gmail.com>
2020-12-02 18:12:41 +0300

    Use useSelector hook for get state

--- PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/CalcResult.js
+++ PrognozMdp/ClientApp/src/components/SimplifiedAnalysis/CalcResult.js
@@ -1,27 +1,52 @@
-﻿import React, { Fragment, useState } from 'react';
+﻿import React, { Fragment } from 'react';
+import { Spinner } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import moment from 'moment'
 
-function CalcResult(props) {
-    const [isLoading, setLoading] = useState(props.loading);
+function CalcResult() {
+    const { loading, flowName, flowWithValues, section, disabledEq, isCurrentDt, dt } = useSelector(
+        state => ({
+            loading: state.calculationReducer.loading,
+            flowName: state.calculationReducer.flowName === "mdp" ? "МДП" : "АДП",
+            flowWithValues: state.calculationReducer.flowWithValues,
+            section: state.calculationReducer.section,
+            disabledEq: state.calculationReducer.disabledEq,
+            isCurrentDt: state.calculationReducer.isCurrentDt,
+            dt: state.calculationReducer.dt
+        }),
+        (prev, next) => prev.loading === next.loading
+    );
 
     return (
         <Fragment>
             {
-                !props.isClear
-            ?
-                props.flowWithVals !== undefined && props.flowWithVals !== null
-                    ? props.flowWithVals.flowCalcValue !== null
+                loading ?
+                    <Spinner
+                        as="span"
+                        animation="border"
+                        size="md"
+                        role="status"
+                        aria-hidden="true"
+                    />
+                :
+                flowWithValues !== undefined && flowWithValues !== null
+                    ? flowWithValues.flowCalcValue !== null
                     ? <div className="result-calc">    
-                                <b>{props.flow}</b> в КС <b>{props.section}</b> для схемы <b>[</b><em>{props.disEq !== "" ? props.disEq : "Нормальная схема"}</em><b>]</b> на момент <b>{
-                                    moment(props.dt).format("HH:mm:ss DD.MM.yyyy")}</b> составляет <b style={
-                                 props.flowWithVals.flowCalcValue > props.flowWithVals.flowCurrentValue ? { color: 'red' } : ""}>{props.flowWithVals
-                                     .flowCalcValue.toFixed(2)}</b> МВт.
+                                <b>{flowName}</b> в КС <b>{section.label}</b> для схемы <b>
+                                    [</b><em>{disabledEq !== "" ? disabledEq : "Нормальная схема"}</em><b>]</b> на момент <b>
+                                    {
+                                        moment(isCurrentDt ? new Date() : dt).format("HH:mm:ss DD.MM.yyyy")
+                                    }
+                                </b> составляет <b style={flowWithValues.flowCalcValue > flowWithValues.flowCurrentValue ? { color: 'red' } : ""}>
+                                    {flowWithValues.flowCalcValue.toFixed(2)}
+                                </b> МВт.
                       </div>
                     : <div>
-                            <span><b>Ремонт неинструктивный.</b></span>        
-                        </div>
+                        <span>
+                            <b>Ремонт неинструктивный.</b>
+                        </span>
+                      </div>
                     : ""
-            : ""
             }
         </Fragment>
     );
