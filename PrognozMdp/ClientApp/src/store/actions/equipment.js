ae66d594df07e48e391398f7225dfa6f0377868f
niksamoyloff <niksamoyloff@gmail.com>
2020-12-02 17:44:29 +0300

    Add and init equipment actions

---
+++ PrognozMdp/ClientApp/src/store/actions/equipment.js
@@ -0,0 +1,36 @@
+﻿import * as types from '../constants/ActionTypes';
+import axios from 'axios';
+
+export const fetchEquipment = (sectionId) => {
+    return async dispatch => {
+        dispatch(fetchEquipmentStarted());
+        await axios.get('SimplifiedAnalysis/GetEquipmentBySection',
+            {
+                params: { sectionId: sectionId }
+            })
+            .then(res => {
+                dispatch(fetchEquipmentSuccess(res.data));
+            })
+            .catch(err => {
+                dispatch(fetchEquipmentFailure(err));
+            });
+    };
+};
+
+const fetchEquipmentStarted = () => ({
+    type: types.FETCH_EQUIPMENT_STARTED
+});
+
+const fetchEquipmentSuccess = data => ({
+    type: types.FETCH_EQUIPMENT_SUCCESS,
+    payload: {
+        data
+    }
+});
+
+const fetchEquipmentFailure = error => ({
+    type: types.FETCH_EQUIPMENT_FAILURE,
+    payload: {
+        error
+    }
+})
