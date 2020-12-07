8b36f292f4e4949fe8956e498cfb4014211088df
niksamoyloff <niksamoyloff@gmail.com>
2020-12-07 09:43:49 +0300

    Add and init DetailedRoot component for detailed analysys

---
+++ PrognozMdp/ClientApp/src/components/DetailedAnalysis/DetailedRoot.js
@@ -0,0 +1,16 @@
+﻿import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import RepairScheme from './RepairScheme';
+import './RepairScheme.css';
+
+function DetailedRoot() {
+    return (
+        <Fragment>
+            <Container fluid className="container-repair-scheme">
+                <RepairScheme />
+            </Container>
+        </Fragment>
+    );
+}
+
+export default DetailedRoot;
