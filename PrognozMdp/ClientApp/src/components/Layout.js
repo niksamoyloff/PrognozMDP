225068a37f3b23f068271895633be6aad75404c8
niksamoyloff <niksamoyloff@gmail.com>
2020-12-07 09:48:46 +0300

    Delete Container component from Layout

--- PrognozMdp/ClientApp/src/components/Layout.js
+++ PrognozMdp/ClientApp/src/components/Layout.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
   render () {
-    return (
+      return (
       <div>
         <NavMenu />
-        <Container>
           {this.props.children}
-        </Container>
       </div>
     );
   }
