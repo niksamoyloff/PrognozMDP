a05a906dd349f9787e12d635358c17db24e81288
niksamoyloff <niksamoyloff@gmail.com>
2021-01-25 13:55:13 +0300

    Add and init GridUncontrolledSections component

---
+++ PrognozMdp/ClientApp/src/components/DetailedAnalysis/Modes/Result/GridUncontrolledSections.js
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
+
+export function GridUncontrolledSections() {
+    const dispatch = useDispatch();
+
+    const columns = [
+        { name: 'id', title: 'ID' },
+        { name: 'name', title: 'Name' },
+    ];
+    const rows = [
+        { id: 0, product: 'Тест' },
+        { id: 1, product: 'Тест' }
+    ];
+
+    return (
+        <div className="card">
+                <Grid
+                    rows={rows}
+                    columns={columns}
+                >
+                    <Table />
+                    <TableHeaderRow />
+                </Grid>
+            </div>
+    );
+}
