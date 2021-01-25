6edbb7c33fbd265f6858da9e86dd1d88a7d80efc
niksamoyloff <niksamoyloff@gmail.com>
2021-01-25 14:12:29 +0300

    Add redux

--- PrognozMdp/ClientApp/src/store/store.js
+++ PrognozMdp/ClientApp/src/store/store.js
@@ -1,7 +1,18 @@
-﻿import { createStore, applyMiddleware } from 'redux';
+﻿import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancer = typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancer(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+);
+
+const store = createStore(reducer, enhancer);
 
 export default store;
