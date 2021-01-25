4e8cd82a46c2b9369725c0441a5b6a34060d7a16
niksamoyloff <niksamoyloff@gmail.com>
2021-01-25 13:58:39 +0300

    Init RepairScheme component

--- PrognozMdp/ClientApp/src/components/DetailedAnalysis/RepairScheme.js
+++ PrognozMdp/ClientApp/src/components/DetailedAnalysis/RepairScheme.js
@@ -1,17 +1,22 @@
-﻿import React from 'react';
-import { ReactSVG } from 'react-svg';
-import { render } from 'react-dom';
-import scheme from './scheme/scheme.svg'
+﻿import React, { Fragment, useState, useEffect, useReducer } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import schemeUrl from './scheme/scheme.svg'
 import { Container, Spinner } from 'react-bootstrap';
+import * as d3 from 'd3';
+import $ from 'jquery';
+import { fetchNetItemNameById, removeFromSelectedItems } from '../../store/actions/detailed/networkTopology';
+import store from '../../store/store';
+import { NetworkItemsAccordion } from './Modes/ChangeNetworkTopology/NetworkItemsAccordion';
 
-function SchemeLoader() {
+const SchemeLoader = () => {
     return (
         <Container fluid className="h-100">
             <div className="row h-100 justify-content-center align-items-center" style={{marginTop: 200}}>
+                <span>Загрузка схемы&nbsp;</span>
                 <Spinner
                     as="span"
                     animation="border"
-                    size="md"
+                    size="sm"
                     role="status"
                     aria-hidden="true"
                 />    
@@ -20,14 +25,105 @@ function SchemeLoader() {
     );
 }
 
-function RepairScheme() {
+export function RepairScheme() {
+    const [firstUpdate, setFirstUpdate] = useState(true);
+    const dispatch = useDispatch();
+    const { loading, selectedNetItems, hasError } = useSelector(
+        state => ({
+            loading: state.detailedNetworkTopologyReducer.loading,
+            selectedNetItems: state.detailedNetworkTopologyReducer.selectedNetItems,
+            hasError: state.detailedNetworkTopologyReducer.error !== null ? true : false
+        })
+        //(prev, next) => prev.loading === next.loading
+    );
+
+    useEffect(() => {
+        if (firstUpdate) {
+            $("#schemeSvg").load(schemeUrl);
+            setFirstUpdate(false);
+        }
+        console.log(selectedNetItems);
+    }, [loading, firstUpdate, selectedNetItems]);
+
+    function handleGetElementDataById(target, id) {
+        dispatch(fetchNetItemNameById(id)).then(() => {
+            if (!store.getState().detailedNetworkTopologyReducer.selectedNetItems.some(item => item.id === id)) // ???
+                handleShowPopover(target);
+            else
+                target.attr("class", "clicked");
+        });
+    }
+
+    function handleRemoveFromSelectedItems(id) {
+        dispatch(removeFromSelectedItems(id));
+    }
+
+    function handleMouseMove(evt) {
+        handleZoom();
+        handleGetElementByClick(evt);
+    }
+
+    function handleZoom() {
+        const svg = d3.select("svg");
+        const scheme = svg.select("#layer1");
+        svg.call(d3.zoom()
+            .scaleExtent([0, 4])
+            .on("zoom", function({ transform }) {
+                scheme.attr("transform", transform);
+            })
+        );
+    }
+
+    function handleShowPopover(target) {
+        target.popover({
+            trigger: 'focus',
+            placement: 'top',
+            title: 'Ошибка!',
+            content: 'Нет данных о сетевом оборудовании.',
+            delay:
+            {
+                "show": 500,
+                "hide": 100
+            }
+        }).popover("show");
+
+        target.on('shown.bs.popover', function () {
+            setTimeout(function() {
+                $('.popover').popover('hide'); 
+            },1500);
+        });
+    }
+
+    function handleGetElementByClick(evt) {
+        const target = $(evt.target);
+        const id = target.attr("id");
+        if (typeof id !== typeof undefined && id !== false) {
+            if (id.indexOf("path") !== -1) {
+                target.css("cursor", "pointer");
+                target.off().click(function() {
+                    if (!target.hasClass("clicked")) {
+                        handleGetElementDataById(target, id);
+                    } else {
+                        target.attr("class", "");
+                        handleRemoveFromSelectedItems(id);
+                    }
+                });
+            }
+        }
+    }
+
     return (
-        //<ReactSVG 
-        //    src={scheme} 
-        //    loading={() => <SchemeLoader/>}
-        ///>
-        <object type="image/svg+xml" data={scheme} />
+        <Fragment>
+            <div id="schemeSvg" onMouseMove={handleMouseMove}></div>
+            {
+                selectedNetItems.length > 0 ? <NetworkItemsAccordion /> : ""
+            }
+        </Fragment>
     );
 }
-//render(<ReactSVG src="scheme.svg" />, document.getElementsByClassName('container'));
-export default RepairScheme;
+//<object 
+//    id="svgScheme"
+//    type="image/svg+xml" 
+//    data={scheme}
+//    onClick={() => alert("object")}
+///>
