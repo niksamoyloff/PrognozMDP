0b99be1e42ce6e8d516712a661608eb32ce34d61
niksamoyloff <niksamoyloff@gmail.com>
2021-01-25 13:55:51 +0300

    Add and init DetailedAnalysisMode component

---
+++ PrognozMdp/ClientApp/src/components/DetailedAnalysis/DetailedAnalysisMode.js
@@ -0,0 +1,40 @@
+﻿import React, { useState, useEffect } from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleShowMode } from '../../store/actions/detailed/common'
+
+export function DetailedAnalysisMode(props) {
+    const dispatch = useDispatch();
+    const [currentMode, setCurrentMode] = useState({});
+
+    const detailedAnalysisModes = [
+        { id: 0, name: "Изменение топологии сети" },
+        { id: 1, name: "Изменение эксплуатационного состояния ПА" },
+        { id: 2, name: "Изменение объема УВ" }
+    ];
+
+    useEffect(() => {
+        setCurrentMode(detailedAnalysisModes[0]);
+    }, [props.isDetailed]);
+
+    function handleSelectMode(modeId) {
+        setCurrentMode(detailedAnalysisModes[modeId]);
+        dispatch(toggleShowMode(modeId, true));
+    }
+
+    return (
+        <DropdownButton
+            title="Режим"
+            variant="outline-light"
+            disabled={!props.isDetailed}
+            onSelect={handleSelectMode}
+        >
+        {
+          detailedAnalysisModes.map((mode, i) => (
+            <Dropdown.Item key={i} eventKey={i} active={mode.id === currentMode.id ? true : false}>
+                {mode.name}
+            </Dropdown.Item>
+        ))}
+        </DropdownButton>
+    );
+}
