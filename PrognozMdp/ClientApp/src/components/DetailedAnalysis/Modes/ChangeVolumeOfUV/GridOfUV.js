42a949082c2918a8cdace770c384bebf9c3f0f4f
niksamoyloff <niksamoyloff@gmail.com>
2021-01-25 13:49:48 +0300

    Add and init GridOfUV component

---
+++ PrognozMdp/ClientApp/src/components/DetailedAnalysis/Modes/ChangeVolumeOfUV/GridOfUV.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
+
+export function GridOfUV() {
+    const dispatch = useDispatch();
+
+    const columns = [
+        { name: 'id', title: 'ID' },
+        { name: 'product', title: 'Product' },
+        { name: 'owner', title: 'Owner' },
+    ];
+    const rows = [
+        { id: 0, product: 'UV', owner: 'UV' },
+        { id: 1, product: 'UV', owner: 'UV' },
+    ];
+
+    return (
+        <div className="card">
+                <Grid
+                    rows={rows}
+                    columns={columns}
+                >
+                    <Table />
+                    <TableHeaderRow />
+                </Grid>
+            </div>
+    );
+}
